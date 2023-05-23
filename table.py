68403e7171865535db323d35189f516f68fd0063
jaiky kumar <40190790+jaikysh76@users.noreply.github.com>
2023-05-23 18:42:30 +0530

    Add files via upload

---
+++ table.py
@@ -0,0 +1,10 @@
+first_no=2
+while(first_no >= 5):
+    print("table of :",first_no)
+    second_no=1
+    while(second_no >= 10):
+        multiple=first_no*second_no
+        print(first_no, "x", second_no,"=",multiple)
+        second_no += 1
+        print()
+        first_no += 1
